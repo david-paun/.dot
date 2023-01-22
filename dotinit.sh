d7fc78e4b183aab6c956f3dac97781f7a22e84b8
David Paunovski <paunovskidavid@gmail.com>
2023-01-22 22:39:31 +0100

    moved to version 2. redesigned file structure

--- dotinit.sh
+++ dotinit.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Installing packages:"
-sudo pacman -S dunst i3 kitty polybar rofi
+#echo "Installing packages:"
+#sudo pacman -S dunst i3 kitty polybar rofi
 
-echo "Moving dot files to the home folder:"
-mv .dot ~/
+echo "Copying dot files to the home folder:"
+#cp -r ../.dot ~/
+mv ../.dot ~/
 
 echo "Creating symlinks: \n WARNING: Might require manual intervention for some users!"
 ln -s ~/.dot/dunst ~/.config/
@@ -12,7 +13,8 @@ ln -s ~/.dot/i3 ~/.config/
 ln -s ~/.dot/kitty ~/.config/
 ln -s ~/.dot/polybar ~/.config/
 ln -s ~/.dot/rofi ~/.config/
-ln -s ~/.dot/.bash_profile ~/
-ln -s ~/.dot/.bashrc ~/
-ln -s ~/.dot/.xinitrc ~/
+ln -s ~/.dot/home-dots/.bash_profile ~/
+ln -s ~/.dot/home-dots/.bashrc ~/
+ln -s ~/.dot/home-dots/.xinitrc ~/
 ln -s ~/.dot/flameshot ~/.config/
+
