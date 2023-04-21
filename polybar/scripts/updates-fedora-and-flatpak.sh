622625f3a302fa6176a95681b2a7199580252644
David Paunovski <paunovskidavid@gmail.com>
2023-04-22 01:00:55 +0200

    fedora updates have handler for no internet, as  well as supressed warnings

--- polybar/scripts/updates-fedora-and-flatpak.sh
+++ polybar/scripts/updates-fedora-and-flatpak.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-    flatpakUpdates=$(flatpak update | grep -Ec '[0-9]+\..*$');
-    dnfUpdates=$(dnf check-update| grep -Ec ' updates$');
-    echo $(($flatpakUpdates + $dnfUpdates))
+if ! nc -zw1 google.com 443 &>/dev/null; then
+  echo ""
+  exit 1
+fi
+
+flatpakUpdates=$(flatpak update | grep -Ec '[0-9]+\..*$')
+dnfUpdates=$(dnf check-update | grep -Ec ' updates$')
+echo $(($flatpakUpdates + $dnfUpdates))
